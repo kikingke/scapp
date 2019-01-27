5247180595f966d1e318eaf85263b7503865ff3b
Enrique <eabucam@gmail.com>
2019-01-26 22:05:49 -0600

    jj

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var app = angular.module('myapp',['ngRoute']);
      var ap = firebase.initializeApp(config);
 
       const auth = firebase.auth();
-      const db = firebase.firestore(ap);
+      const db = firebase.firestore();
+
+
 //firebase
 
 //routing
@@ -30,6 +32,10 @@ app.config(function($routeProvider){
 	.when('/home',{
 		templateUrl:'views/home.html',
 		controller : "homecontroller"
+	})
+	.when('/add',{
+		templateUrl:'views/add.html',
+		controller : "addcontroller"
 	})
 	.when('/profile',{
 		templateUrl:'views/profile.html',
@@ -51,41 +57,16 @@ app.config(function($routeProvider){
 
 
 //controllers
-app.controller('homecontroller',  ['$scope','$location','$window', function($scope,$location,$window) {
+app.controller('homecontroller',  ['$scope','$location','$window', function($scope, $location, $window) {
 			
-				db.collection('guides').get()
-				.then(function(querySnapshot) {
-					//console.log(querySnapshot.docs);
-				    querySnapshot.forEach(function(doc) {
-				        // doc.data() is never undefined for query doc snapshots
-				        console.log(doc.id, " => ", doc.data());
-				    });
+			function escucha(){
+				db.collection('guides').onSnapshot(snapshot => {
+				 var datosfrescos = snapshot.docs;
+				    $scope.items.push(datosfrescos);
+				    console.log(datosfrescos);
 				})
-
-				.catch(function(error){
-					 console.log("Error getting documents ",error);
-				});
-
-
-			// db.collection('guides').get().then(snapshot => {
-			// 	console.log(snapshot.docs);
-			// });
-
-
-			// var docRef = db.collection("guides").doc();
-
-			// docRef.get().then(function(doc) {
-			//     if (doc.exists) {
-			//         console.log("Document data:", doc.data());
-			//     } else {
-			//         // doc.data() will be undefined in this case
-			//         console.log("No such document!");
-			//     }
-			// }).catch(function(error) {
-			//     console.log("Error getting document:", error);
-			// });
-
-
+			}
+			
 			  $scope.cerrarsesion = function() { 
 			  		auth.signOut().then(() => {
 							console.log("USUARIO TEFUE");
@@ -95,26 +76,60 @@ app.controller('homecontroller',  ['$scope','$location','$window', function($sco
 			  		})
 			  }
 
-
-
 			    auth.onAuthStateChanged(user => {
 						//console.log(user);
 						if (user){
-					     //  console.log("USUARIO LOGUEADITO");
-					     //    $scope.reset = function() {
-						    //     $scope.email = '';
-						    //     $scope.password = '';
-						    //     // Todo: Reset field to pristine state, its initial state!
-						    // };
-					     //   $window.location.href = '#!/home';
-					     // return true;
+
+							$scope.usuarioactivo = user.email;
+						
+                            escucha();
 						}else{
-					      console.log("USUARIO no LOGUEADITO");
-					      // return false;
-					        
+					      console.log("USUARIO NO LOGUEADITO");
+				        
 					       $window.location.href = '#!/';
 						} 
 				  })
+
+			   // var ref = new Firebase('https://URL.firebaseio.com/users');
+
+				$scope.items = [];
+
+				db.collection('guides').get()
+				 .then(function(querySnapshot) {
+				    querySnapshot.forEach(function(doc) {
+				        var datos = doc.data();
+				        $scope.items.push(datos);
+				        console.log(doc.id, " => ", doc.data());
+				    });
+				    $scope.$apply(); //let angular know to trigger $digest loop to update the DOM.
+				});
+
+
+
+}]);
+
+
+app.controller('addcontroller', ['$scope','$location','$window', function($scope,$location,$window) {
+
+
+			  $scope.additem = function() { 
+			    // var title =;
+			     var titulo = $scope.title;
+			     var contenido = $scope.content;
+				  
+			     //console.log(correo);
+                db.collection('guides').add({
+                	title: titulo ,
+                	content: contenido
+                }).then(() => {
+                	$window.location.href = '#!/home';
+                	console.log("Dato Agregado");
+                }).catch(error =>{
+                	console.log(error.message);
+                })
+			     	
+
+			  }
 
 
 
@@ -133,7 +148,9 @@ app.controller('registercontroller', ['$scope','$location','$window', function($
 
 			     	auth.createUserWithEmailAndPassword(correo, contras).then(cred => {
 			     			console.log(cred);
-			     	})
+			     	}).catch(error =>{
+                	console.log(error.message);
+                    })
 
 			  }
 
@@ -152,22 +169,14 @@ app.controller('logincontroller', ['$scope','$location','$window', function($sco
 
 			     auth.signInWithEmailAndPassword(correo, contras).then(cred => {
 			     	console.log("LOGUEO" + cred.user);
-			     	// if (estadodeusuario()){
-         //                  $location.path('/home');
-         //                  console.log("pase por location");
-			     	// }else{
-         //                  $location.path('/');
-			     	// }
-			     	 
-			     	 
-			     })
+
+			     }).catch(error =>{
+                	console.log(error.message);
+                 })
 				  
 
 
 			  }
-
-
-			  
 
 
 				  auth.onAuthStateChanged(user => {
@@ -196,30 +205,13 @@ app.controller('logincontroller', ['$scope','$location','$window', function($sco
 app.controller('profilecontroller', ['$scope','$location','$window', function($scope,$location,$window) {
 
 
-			  // $scope.register = function() { 
-			  //    var correo = $scope.email;
-			  //    var usuario = $scope.username;
-			  //    var contras = $scope.password;
-				  
-			  //    //console.log(correo);
-
-			  //    	auth.createUserWithEmailAndPassword(correo, contras).then(cred => {
-			  //    			console.log(cred);
-			  //    	})
-
-			  // }
-
 			   auth.onAuthStateChanged(user => {
 						//console.log(user);
 						if (user){
 					      console.log("USUARIO LOGUEADITO");
-					     //    $scope.reset = function() {
-						    //     $scope.email = '';
-						    //     $scope.password = '';
-						    //     // Todo: Reset field to pristine state, its initial state!
-						    // };
-					     //   $window.location.href = '#!/home';
-					     // return true;
+	                        $scope.usuarioactivo = user.email;
+	                      console.log(user.email);
+					     //$scope.datosaperfil = user.email;
 						}else{
 					      console.log("USUARIO DESLOGUEADITO");
 					      // return false;
@@ -231,17 +223,3 @@ app.controller('profilecontroller', ['$scope','$location','$window', function($s
 
 
 }]);
-
-//controllers
-// function estadodeusuario(){
-// 	auth.onAuthStateChanged(user => {
-// 	console.log(user);
-// 	if (user){
-//       console.log("Usuario logueado");
-//       return true;
-// 	}else{
-//       console.log("Usuario no logueado");
-//       return false;
-// 	} 
-//   })
-// }
